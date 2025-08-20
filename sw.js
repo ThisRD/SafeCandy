80a2770d2480fc79d7cde822ad72f16c01dc7488
ThisRD <raydson.isidorio.silva@gmail.com>
2025-08-20 17:03:31 -0300

    20/08/2025

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'pwa-safe-candy-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/manifest.json',
-    '/icons/logotipo.png',
-    '/icons/logo.png'
+    '/SafeCandy/',
+    '/SafeCandy/index.html',
+    '/SafeCandy/style.css',
+    '/SafeCandy/manifest.json',
+    '/SafeCandy/icons/logotipo.png',
+    '/SafeCandy/icons/logo.png'
 ];
 
 self.addEventListener('install', event => {
